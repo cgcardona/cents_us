2bbb1dfb379b679583cca3b16d3784b29b3bdbf7
Carlos Cardona <cgcardona@gmail.com>
2013-01-24 07:22:04 -0800

    stub rails app with underscore and haml gems added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CentsUs::Application.config.secret_token = '297a7a95ddbf9729a280aaef4ecf6316d56e12cbb3a4f7f415630d7f824b751250fc4ffffc6f135f3860767cb61a829e3f2f5f9451d065ca3a832a3d248844d8'
